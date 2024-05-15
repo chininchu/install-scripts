98585bc7a2dada1d3fffab30afa16f912adc8996
chininchu <andrew.c.chu@outlook.com>
2024-05-16 00:50:10 +0500

    Added a README.d

---
+++ README.md
@@ -0,0 +1,3 @@
+#Script
+
+# powershell -ExecutionPolicy Bypass -Command "& { (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1') } -WhatIf" 
--- application-scripts.ps1
+++ application-scripts.ps1
@@ -1,13 +1,7 @@
-
-
-
-
 # Set Execution Policy to allow running scripts
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-#Script
 
-# powershell -ExecutionPolicy Bypass -Command "& { (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1') } -WhatIf" 
 
 # Install Chocolatey packages
 choco install zoom slack googlechrome protonvpn -y 
