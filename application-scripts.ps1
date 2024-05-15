f1ae436dc8304a1c0b8a3c51aa546ca7423d1507
chininchu <andrew.c.chu@outlook.com>
2024-05-16 00:32:46 +0500

    Added the script

--- application-scripts.ps1
+++ application-scripts.ps1
@@ -1,3 +1,9 @@
+
+#Script
+
+# powershell -ExecutionPolicy Bypass -Command "& { (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1') } -WhatIf" 
+
+
 # Set Execution Policy to allow running scripts
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
