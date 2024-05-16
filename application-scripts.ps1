bb8f89dc6db8a9ddea5bad307bf36962e1c6e1d0
chininchu <andrew.c.chu@outlook.com>
2024-05-16 14:23:59 +0500

    Added the ability to install Node js, VS Code and Salesforce CLI

--- application-scripts.ps1
+++ application-scripts.ps1
@@ -1,8 +1,9 @@
-# Set Execution Policy to allow running scripts
+# Set Execution Policy to allow running scripts (This will still run even with -WhatIf)
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
+# Install Chocolatey packages (with -WhatIf)
+choco install zoom slack googlechrome protonvpn nodejs openjdk17 vscode -y -WhatIf
 
-
-# Install Chocolatey packages
-choco install zoom slack googlechrome protonvpn -y 
+# Install Salesforce CLI using npm (with -WhatIf)
+npm install @salesforce/cli --global -WhatIf
 
