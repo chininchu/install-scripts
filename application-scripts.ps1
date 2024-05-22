88e28b9643b9391ab862896752090ab4349f7fda
chininchu <andrew.c.chu@outlook.com>
2024-05-22 18:03:28 +0500

    modified

--- application-scripts.ps1
+++ application-scripts.ps1
@@ -322,3 +322,13 @@
 #   Install-ChocolateyPackage slack -Source https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1 -Version 4.38.125
 #   Install-ChocolateyPackage googlechrome -Source https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1 -Version 125.0.6422.76
 #   Install-ChocolateyPackage temurin17 -Source https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1 -Version 17.0.10
+
+
+choco install nodejs-lts --version 20.13.1 -y
+choco install git --version 2.45.1 -y
+choco install vscode --version 1.89.1 -y
+choco install openssh --version 8.0.0.1 -y
+choco install zoom --version 6.0.4.38135 -y
+choco install slack --version 4.38.125 -y
+choco install googlechrome --version 125.0.6422.76 -y
+choco install temurin17 --version 17.0.10 -y
