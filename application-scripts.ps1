0a53731dabf3c2f338b2d9a52e3d834ba0649789
chininchu <andrew.c.chu@outlook.com>
2024-05-22 17:40:49 +0500

    modified

--- application-scripts.ps1
+++ application-scripts.ps1
@@ -45,11 +45,20 @@
 #   Install-ChocolateyPackage googlechrome -Source https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1 -Version 125.0.6422.76
 #   Install-ChocolateyPackage temurin17 -Source https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1 -Version 17.0.10
 
-choco upgrade nodejs-lts -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'20.13.1'" [other options]
-choco upgrade git -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'2.45.1'" [other options]
-choco upgrade vscode -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'1.89.1'" [other options]
-choco upgrade openssh -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'8.0.0.1'" [other options]
-choco upgrade zoom -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'6.0.4.38135'" [other options]
-choco upgrade slack -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'4.38.125'" [other options]
-choco upgrade googlechrome -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'125.0.6422.76'" [other options]
-choco upgrade temurin17 -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'17.0.10'" [other options]
+# choco upgrade nodejs-lts -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'20.13.1'" [other options]
+# choco upgrade git -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'2.45.1'" [other options]
+# choco upgrade vscode -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'1.89.1'" [other options]
+# choco upgrade openssh -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'8.0.0.1'" [other options]
+# choco upgrade zoom -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'6.0.4.38135'" [other options]
+# choco upgrade slack -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'4.38.125'" [other options]
+# choco upgrade googlechrome -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'125.0.6422.76'" [other options]
+# choco upgrade temurin17 -y --source="'https://raw.githubusercontent.com/chininchu/install-scripts/main/application-scripts.ps1'" --version "'17.0.10'" [other options]
+
+choco install nodejs-lts --version 20.13.1 -y
+choco install git --version 2.45.1 -y
+choco install vscode --version 1.89.1 -y
+choco install openssh --version 8.0.0.1 -y
+choco install zoom --version 6.0.4.38135 -y
+choco install slack --version 4.38.125 -y
+choco install googlechrome --version 125.0.6422.76 -y
+choco install temurin17 --version 17.0.10 -y
